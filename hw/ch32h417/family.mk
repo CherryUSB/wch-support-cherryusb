0fd37904482312061881cdfd7f14356929001206
Links <lhd@wch.cn>
2026-09-17 11:19:40 +0800

    Add ch32h417 chips support

---
+++ hw/ch32h417/family.mk
@@ -0,0 +1,84 @@
+# --- Toolchain ---
+PREFIX  := riscv32-wch-elf
+CC      := $(PREFIX)-gcc
+AR      := $(PREFIX)-ar
+OBJCOPY := $(PREFIX)-objcopy
+OBJDUMP := $(PREFIX)-objdump
+SIZE    := $(PREFIX)-size
+
+# --- Include Directories ---
+INCLUDES += \
+    $(FAMILY_DIR)/sdk/Core \
+    $(FAMILY_DIR)/sdk/Debug \
+    $(FAMILY_DIR)/sdk/Peripheral/inc \
+    $(FAMILY_DIR)/system \
+    $(FAMILY_DIR)/libcpu \
+
+# --- Assembly Source Directories ---
+ASM_DIR += \
+	$(FAMILY_DIR)/sdk/Startup \
+	$(FAMILY_DIR)/libcpu \
+
+# --- C Source Directories ---
+SRC_DIR += \
+    $(FAMILY_DIR)/sdk/Core \
+    $(FAMILY_DIR)/sdk/Debug \
+    $(FAMILY_DIR)/sdk/Peripheral/src \
+    $(FAMILY_DIR)/system \
+    $(FAMILY_DIR)/libcpu \
+
+# --- Library Directories ---
+LIB_DIR +=
+
+# --- Assembly Source Files ---
+ASMS += \
+	context_gcc.S \
+	interrupt_gcc.S \
+
+ifeq ($(CORE),V5F)
+CFLAGS += -DCore_V5F
+ASMS += startup_ch32h417_v5f.S
+LDFLAGS += -T "$(CHIP_DIR)/linker_script/Link_v5f.ld"
+else
+CFLAGS += -DCore_V3F
+ASMS += startup_ch32h417_v3f.S
+LDFLAGS += -T "$(CHIP_DIR)/linker_script/Link_v3f.ld"
+endif
+
+# --- C Source Files ---
+SRCS += $(foreach dir,$(SRC_DIR),$(wildcard $(dir)/*.c))
+
+# --- Libraries ---
+LIBS += -lm
+
+# --- Compiler Flags ---
+CFLAGS += \
+	-march=rv32imac_zba_zbb_zbc_zbs_xw \
+	-mabi=ilp32 \
+	-msmall-data-limit=8 \
+	-msave-restore \
+	-Os \
+	-fmessage-length=0 \
+	-fsigned-char \
+	-ffunction-sections \
+	-fdata-sections \
+	-fno-common \
+	-Wunused \
+	-Wuninitialized \
+	-g \
+	-gdwarf-4 \
+	-std=gnu11 \
+	$(addprefix -I,$(INCLUDES))\
+	$(addprefix -L,$(LIB_DIR))\
+
+# --- Linker Flags ---
+LDFLAGS += \
+	$(CFLAGS) \
+	$(LIBS) \
+	-nostartfiles \
+	-Xlinker \
+	--gc-sections \
+	-Wl,--print-memory-usage \
+	-Wl,-Map,$(MAP_FILE) \
+	--specs=nano.specs \
+	--specs=nosys.specs \
